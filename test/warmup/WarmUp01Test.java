958a3742e3b063e3203e7c12964f8274999c646c
Connor Reid <car349@txstate.edu>
2018-02-06 14:35:00 -0600

    Implement test cases.

--- test/warmup/WarmUp01Test.java
+++ test/warmup/WarmUp01Test.java
@@ -7,6 +7,9 @@ public class WarmUp01Test
     public static void main(String[] args)
     {
         WarmUp01Test.test01();
+        WarmUp01Test.test02();
+        WarmUp01Test.test03();
+        WarmUp01Test.test04();
     }
 
     private static void test01()
@@ -15,6 +18,60 @@ public class WarmUp01Test
         String name = "WarmUp01Test.test01";
         String in = "abscacd";
         String expectedOut = "dasaccb";
+        String out = warmUp01.run(in);
+
+        try
+        {
+            TestUtil.runTest(name, in, out, expectedOut);
+        }
+        catch (Exception exc)
+        {
+            System.out.print("\n\n" + exc);
+        }
+    }
+
+    private static void test02()
+    {
+        WarmUp01 warmUp01 = new WarmUp01();
+        String name = "WarmUp01Test.test02";
+        String in = "allyourbasearebelongtous";
+        String expectedOut = "utnlbrearolasogoeeasbuyl";
+        String out = warmUp01.run(in);
+
+        try
+        {
+            TestUtil.runTest(name, in, out, expectedOut);
+        }
+        catch (Exception exc)
+        {
+            System.out.print("\n\n" + exc);
+        }
+    }
+
+    private static void test03()
+    {
+        WarmUp01 warmUp01 = new WarmUp01();
+        String name = "WarmUp01Test.test03";
+        String in = "Igetpaidforcodethatworksnotfortests";
+        String expectedOut = "sstotnkotheorfipeIterfosrwatdcodatg";
+        String out = warmUp01.run(in);
+
+        try
+        {
+            TestUtil.runTest(name, in, out, expectedOut);
+        }
+        catch (Exception exc)
+        {
+            System.out.print("\n\n" + exc);
+        }
+    }
+
+    private static void test04()
+    {
+        WarmUp01 warmUp01 = new WarmUp01();
+        String name = "WarmUp01Test.test04";
+        String in = "thistestwillencryptthismessagebysplittingthemessageintotwosubsetsofevenandoddindicesandthenreversingbothstringsandfinallyconcatenating";
+        String expectedOut = "ntntcoylnfnsnrstbnseenhdaeindonnvfsebswoneasmhgitlsbgseshtycelwstitgiaeanclaidagithogirvretnscdiddaeeotsuottigseetntipyeasmitprnlitesh";
         String out = warmUp01.run(in);
 
         try
--- test/warmup/WarmUp02Test.java
+++ test/warmup/WarmUp02Test.java
@@ -8,6 +8,7 @@ public class WarmUp02Test
     {
         WarmUp02Test.test01();
         WarmUp02Test.test02();
+        WarmUp02Test.test03();
     }
 
     private static void test01()
@@ -34,6 +35,24 @@ public class WarmUp02Test
         String name = "WarmUp02Test.test02";
         String in = "10010";
         int expectedOut = 10;
+        int out = warmUp02.run(in);
+
+        try
+        {
+            TestUtil.runTest(name, in, String.valueOf(out), String.valueOf(expectedOut));
+        }
+        catch (Exception exc)
+        {
+            System.out.print("\n\n" + exc);
+        }
+    }
+
+    private static void test03()
+    {
+        WarmUp02 warmUp02 = new WarmUp02();
+        String name = "WarmUp02Test.test03";
+        String in = "0001011010001110111010111011101001001110111001111110100010011011";
+        int expectedOut = 604;
         int out = warmUp02.run(in);
 
         try
