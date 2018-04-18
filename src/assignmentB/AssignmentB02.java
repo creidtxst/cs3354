d907a8d4150583e6a9152801efc305f19f4a5087
Connor Reid <car349@txstate.edu>
2018-04-18 13:18:21 -0500

    Assignment B Problem 1. Hook up table selection listener. Update table header when selection occurs.

--- src/assignmentB/AssignmentB02.java
+++ src/assignmentB/AssignmentB02.java
@@ -1,108 +1,236 @@
 package assignmentB;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-public class AssignmentB02 extends JFrame
+public class AssignmentB02
 {
-    Calendar cal = new GregorianCalendar();
-    DefaultTableModel model;
-    JLabel label;
+    // Constants
+    private static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    private static final String FRAME_HEADER_TEXT = "Appointment Calendar";
+    private static final Dimension DEFAULT_FRAME_DIMENSION = new Dimension(640, 480);
+    private static final Dimension MIN_FRAME_DIMENSION = new Dimension(640, 480);
+    private static final Dimension MAX_FRAME_DIMENSION = new Dimension(640, 480);
+    private static final int TABLE_ROW_HEIGHT = 30;
+
+    // Static fields
+    private static JFrame parentFrame;
+    private static Calendar cal = new GregorianCalendar();
+    private static DefaultTableModel tableModel;
+    private static JLabel tableHeaderLabel;
+    private static JTable table;
+    private static int selectedDay;
+    private static boolean isProgrammaticallySelected = false;
+
+    // Handler
+    private static SharedListSelectionHandler sharedListSelectionHandler;
 
     //TODO: add text box to add/view appointments
 
-    AssignmentB02()
+    private static void init()
     {
-        this.setTitle("Appointment Calendar");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(640, 480);
-        //this.setResizable(false);
-        this.setVisible(true);
+        // Create parent frame
+        parentFrame = new JFrame();
+
+        parentFrame.setTitle(FRAME_HEADER_TEXT);
+        parentFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        parentFrame.setSize(DEFAULT_FRAME_DIMENSION);
+        parentFrame.setMinimumSize(MIN_FRAME_DIMENSION);
 
         //TODO: fix formatting... need fixed table that doesn't resize with window
-        //this.BoarderLayout() ??
-        this.setLayout(new FlowLayout());
+        parentFrame.setLayout(new FlowLayout());
 
-        label = new JLabel();
-        label.setHorizontalAlignment(SwingConstants.CENTER);
+        // Create table frame
+        JPanel tablePanel = new JPanel();
+        tablePanel.setLayout(new BoxLayout(tablePanel, BoxLayout.Y_AXIS));
 
+        // Create buttons
         JButton prevMonthButton = new JButton("<<");
         JButton nextMonthButton = new JButton(">>");
 
-        prevMonthButton.addActionListener(ae -> {
-            cal.add(Calendar.MONTH, -1);
-            updateCalendarState();
-        });
-
-        nextMonthButton.addActionListener(ae -> {
-            cal.add(Calendar.MONTH, +1);
-            updateCalendarState();
-        });
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        panel.add(prevMonthButton, FlowLayout.LEFT);
-        panel.add(label, BorderLayout.CENTER);
-        panel.add(nextMonthButton, FlowLayout.RIGHT);
-
-        String[] days = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-
-        model = new DefaultTableModel(null, days)
+        prevMonthButton.addActionListener(new ActionListener()
         {
             @Override
-            public boolean isCellEditable(int row, int column) {
+            public void actionPerformed(ActionEvent ae)
+            {
+                cal.add(Calendar.MONTH, -1);
+                updateAfterNextPrevButtonPress();
+            }
+        });
+
+        nextMonthButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                cal.add(Calendar.MONTH, +1);
+                updateAfterNextPrevButtonPress();
+            }
+        });
+
+        // Create table header
+        JPanel tableHeaderPanel = new JPanel();
+        tableHeaderPanel.setLayout(new FlowLayout());
+        tableHeaderPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // Add Previous Month button
+        tableHeaderPanel.add(prevMonthButton, FlowLayout.LEFT);
+
+        // Add header label
+        tableHeaderLabel = new JLabel();
+        tableHeaderLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        tableHeaderPanel.add(tableHeaderLabel, FlowLayout.CENTER);
+
+        // Add Next Month button
+        tableHeaderPanel.add(nextMonthButton, FlowLayout.RIGHT);
+
+        // Setup table model
+        tableModel = new DefaultTableModel(null, DAYS)
+        {
+            @Override
+            public boolean isCellEditable(int row, int column)
+            {
                 return false;
             }
         };
 
-        JTable table = new JTable(model);
-        JScrollPane pane = new JScrollPane(table);
+        // Create table
+        table = new JTable(tableModel);
         table.setCellSelectionEnabled(true);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        table.setRowHeight(20);
-        TableColumnModel columnModel = table.getColumnModel();
+        table.setRowHeight(TABLE_ROW_HEIGHT);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 
-        for (int i = 0; i < 7; i++)                                 // Sets calendar columns to a fixed width (40)
-            columnModel.getColumn(i).setPreferredWidth(40);
+        // Set up table model listener
+        sharedListSelectionHandler = new SharedListSelectionHandler();
+        ListSelectionModel rowSelMod = table.getSelectionModel();
+        ListSelectionModel colSelMod = table.getColumnModel().getSelectionModel();
+        rowSelMod.addListSelectionListener(sharedListSelectionHandler);
+        colSelMod.addListSelectionListener(sharedListSelectionHandler);
 
-        this.add(panel,BorderLayout.NORTH);
-        this.add(pane,BorderLayout.CENTER);
+        // Attach table to scroll pane
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        // Put it all together...
+        tablePanel.add(tableHeaderPanel);
+        tablePanel.add(scrollPane);
+        parentFrame.add(tablePanel, FlowLayout.LEFT);
 
-        this.updateCalendarState();
+        // Init table state
+        updateCalendarState();
+
+        // Programmatically select current day in table
+        selectDayInTable(Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+        // Parent frame is ready to be displayed...
+        parentFrame.pack();
+        parentFrame.setVisible(true);
     }
 
-    void updateCalendarState()
+    private static void updateAfterNextPrevButtonPress()
+    {
+        updateCalendarState();
+        selectDayInTable(cal.get(Calendar.DAY_OF_WEEK));
+        updateTableHeaderText();
+    }
+
+    private static void updateCalendarState()
     {
         cal.set(Calendar.DAY_OF_MONTH, 1);
-        String month = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
-        int year = cal.get(Calendar.YEAR);
-        label.setText(month + " " + year);
 
         int firstDay = cal.get(Calendar.DAY_OF_WEEK);
         int totalDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
         int weeks = cal.getActualMaximum(Calendar.WEEK_OF_MONTH);
 
-        model.setRowCount(0);
-        model.setRowCount(weeks);
+        isProgrammaticallySelected = true;
+        tableModel.setRowCount(0);
+        tableModel.setRowCount(weeks);
 
         int i = (firstDay - 1);
         for (int day = 1; day <= totalDays; day++)
         {
-            model.setValueAt(day, i / 7, i % 7);
-            i = i + 1;
+            tableModel.setValueAt(day, i / 7, i % 7);
+            i++;
         }
+    }
 
+    private static void selectDayInTable(int day)
+    {
+        int i = (day - 1);
+        int rowIndex = i / 7;
+        int columnIndex = i % 7;
+        boolean toggle = false;
+        boolean extend = false;
+        isProgrammaticallySelected = true;
+        table.changeSelection(rowIndex, columnIndex, toggle, extend);
+    }
+
+    private static void updateTableHeaderText()
+    {
+        String month = cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US);
+        int year = cal.get(Calendar.YEAR);
+        String t = month + " " + selectedDay + ", " + year;
+        tableHeaderLabel.setText(t);
+    }
+
+    private static class SharedListSelectionHandler implements ListSelectionListener
+    {
+        @Override
+        public void valueChanged(ListSelectionEvent e)
+        {
+            if (isProgrammaticallySelected)
+            {
+                isProgrammaticallySelected = false;
+                return;
+            }
+
+            int[] rows = table.getSelectedRows();
+            int selectedRow = rows.length > 0 ? rows[0] : 0;
+
+            int[] cols = table.getSelectedColumns();
+            int selectedColumn = cols.length > 0 ? cols[0] : 0;
+
+            try
+            {
+                selectedDay = (Integer) table.getValueAt(selectedRow, selectedColumn);
+            }
+            catch (ArrayIndexOutOfBoundsException exc)
+            {
+                System.out.print("\n" + exc.toString());
+                selectedDay = 1;
+                selectDayInTable(selectedDay);
+            }
+            catch (NullPointerException npe)
+            {
+                System.out.print("\n" + npe.toString());
+            }
+
+            updateTableHeaderText();
+        }
+    }
+
+    private static void createAndShowGUI()
+    {
+        init();
     }
 
     public static void main(String args[])
     {
-        AssignmentB02 calendar = new AssignmentB02();
+        // Run...
+        javax.swing.SwingUtilities.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                createAndShowGUI();
+            }
+        });
     }
 }
